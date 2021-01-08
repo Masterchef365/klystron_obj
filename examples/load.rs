684d71d98066315c79a2f35ebafe0ee0bbcf2e46
Duncan Freeman <dfreeman@lucidyne.com>
2021-01-08 02:27:14 -0800

    Load multiple textured OBJs in example

--- examples/load.rs
+++ examples/load.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use klystron::{
     runtime_3d::{launch, App},
     DrawType, Engine, FramePacket, Object, UNLIT_FRAG, UNLIT_VERT, Sampling,
@@ -9,69 +9,83 @@ use std::fs::{File, read};
 use std::io::BufReader;
 
 struct MyApp {
-    tris: Object,
+    objects: Vec<Object>,
     time: f32,
 }
 
 impl App for MyApp {
     const NAME: &'static str = "MyApp";
 
-    type Args = (String, String);
+    type Args = Vec<(String, String)>;
 
-    fn new(engine: &mut dyn Engine, (obj_path, texture_path): Self::Args) -> Result<Self> {
-        let file = BufReader::new(File::open(obj_path)?);
-        let obj = parse_obj(file).context("Error parsing OBJ")?;
+    fn new(engine: &mut dyn Engine, models: Self::Args) -> Result<Self> {
+        let mut objects = Vec::new();
 
-        // Read important image data
-        let img = png::Decoder::new(File::open(texture_path)?);
-        let (info, mut reader) = img.read_info()?;
-        assert!(info.color_type == png::ColorType::RGBA);
-        assert!(info.bit_depth == png::BitDepth::Eight);
-        let mut img_buffer = vec![0; info.buffer_size()];
-        assert_eq!(info.buffer_size(), (info.width * info.height * 4) as _);
-        reader.next_frame(&mut img_buffer)?;
-        let texture = engine.add_texture(&img_buffer, info.width, Sampling::Nearest)?;
+        for (obj_path, texture_path) in models {
+            let file = BufReader::new(File::open(obj_path)?);
+            let obj = parse_obj(file).context("Error parsing OBJ")?;
 
-        // Triangles
-        let (vertices, indices) = triangles(&obj)?;
-        let mesh = engine.add_mesh(&vertices, &indices)?;
-        let tri_material = engine.add_material(
-            &read("./shaders/unlit.vert.spv")?, 
-            &read("./shaders/unlit.frag.spv")?, 
-            DrawType::Triangles
-        )?;
-        let tris = Object {
-            transform: Matrix4::identity(),
-            mesh,
-            material: tri_material,
-            texture,
-        };
+            // Read important image data
+            let img = png::Decoder::new(File::open(texture_path)?);
+            let (info, mut reader) = img.read_info()?;
+            assert!(info.color_type == png::ColorType::RGBA);
+            assert!(info.bit_depth == png::BitDepth::Eight);
+            let mut img_buffer = vec![0; info.buffer_size()];
+            assert_eq!(info.buffer_size(), (info.width * info.height * 4) as _);
+            reader.next_frame(&mut img_buffer)?;
+            let texture = engine.add_texture(&img_buffer, info.width, Sampling::Nearest)?;
+
+            // Triangles
+            let (vertices, indices) = triangles(&obj)?;
+            let mesh = engine.add_mesh(&vertices, &indices)?;
+            let tri_material = engine.add_material(
+                &read("./shaders/unlit.vert.spv")?, 
+                &read("./shaders/unlit.frag.spv")?, 
+                DrawType::Triangles
+            )?;
+            objects.push(Object {
+                transform: Matrix4::identity(),
+                mesh,
+                material: tri_material,
+                texture,
+            });
+        }
 
         Ok(Self {
-            tris,
+            objects,
             time: 0.0,
         })
     }
 
     fn next_frame(&mut self, engine: &mut dyn Engine) -> Result<FramePacket> {
-        // Update positions
-        let rotate = Matrix4::from_euler_angles(0.0, self.time, 0.0);
-        self.tris.transform = rotate;
-
         // Update time
         engine.update_time_value(self.time)?;
         self.time += 0.01;
 
         Ok(FramePacket {
-            objects: vec![self.tris],
+            objects: self.objects.clone(),
         })
     }
 }
 
 fn main() -> Result<()> {
     let mut args = std::env::args().skip(1);
-    let obj_path = args.next().context("Requires OBJ path.")?;
-    let texture_path = args.next().context("Requires texture path.")?;
-    let vr = args.next().is_some();
-    launch::<MyApp>(vr, (obj_path, texture_path))
+
+    let vr = match args.next().as_ref().map(|s| s.as_str()) {
+        Some("vr") => true,
+        Some("win") => false,
+        _ => bail!("Requires mode either \"vr\" or \"win\""),
+    };
+
+    let mut models = Vec::new();
+
+    loop {
+        let obj_path = match args.next() {
+            Some(o) => o,
+            None => break,
+        };
+        let texture_path = args.next().context("Requires texture path.")?;
+        models.push((obj_path, texture_path))
+    }
+    launch::<MyApp>(vr, models)
 }
