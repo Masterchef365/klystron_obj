f11b7f528864c77a1715f27f86ba56e300bef7de
Masterchef365 <duncan.freeman1@gmail.com>
2021-04-10 18:42:34 -0700

    Normals

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ use std::collections::{HashMap, HashSet};
 // Don't forget the examples/ !
 
 /// Generate a triangle mesh for this
-pub fn triangles(obj: &RawObj) -> Result<(Vec<Vertex>, Vec<u16>)> {
-    polygon_indices(obj, |indices, poly| -> Result<()> {
+pub fn triangles(obj: &RawObj, color_mode: ColorMode) -> Result<(Vec<Vertex>, Vec<u16>)> {
+    polygon_indices(obj, color_mode, |indices, poly| -> Result<()> {
         match poly.len() {
             3 => Ok(indices.extend(poly.iter().copied())),
             4 => {
@@ -17,19 +17,27 @@ pub fn triangles(obj: &RawObj) -> Result<(Vec<Vertex>, Vec<u16>)> {
                 indices.extend_from_slice(&[poly[0], poly[2], poly[3]]);
                 Ok(())
             }
-            _ => bail!("Polygon is not a triangle or quad"),
+            _ => Ok(()),
         }
     })
 }
 
 /// Whether or not to tessellate quads when generating a wireframe
+#[derive(Copy, Clone, Debug)]
 pub enum QuadMode {
     Keep,
     Tessellate,
 }
 
+/// Which feature to extract and put in the vertex
+#[derive(Copy, Clone, Debug)]
+pub enum ColorMode {
+    Normal,
+    Uv,
+}
+
 /// Generate a wireframe from this OBJ. Capable of preserving quads (see `[crate::QuadMode]`).
-pub fn wireframe(obj: &RawObj, quad_mode: QuadMode) -> Result<(Vec<Vertex>, Vec<u16>)> {
+pub fn wireframe(obj: &RawObj, color_mode: ColorMode, quad_mode: QuadMode) -> Result<(Vec<Vertex>, Vec<u16>)> {
     let mut line_dedup = HashSet::new();
 
     let mut add_line = |indices: &mut Vec<u16>, a: u16, b: u16| {
@@ -40,15 +48,15 @@ pub fn wireframe(obj: &RawObj, quad_mode: QuadMode) -> Result<(Vec<Vertex>, Vec<
         }
     };
 
-    fn quad_check(poly: &[u16]) -> Result<()> {
-        if poly.len() < 3 || poly.len() > 4 {
-            bail!("Polygon is not a triangle or quad");
-        }
+    fn quad_check(_poly: &[u16]) -> Result<()> {
+        //if poly.len() < 3 || poly.len() > 4 {
+            //bail!("Polygon is not a triangle or quad");
+        //}
         Ok(())
     }
 
     match quad_mode {
-        QuadMode::Tessellate => polygon_indices(obj, |indices, poly| -> Result<()> {
+        QuadMode::Tessellate => polygon_indices(obj, color_mode, |indices, poly| -> Result<()> {
             quad_check(poly)?;
 
             add_line(indices, poly[0], poly[1]);
@@ -63,7 +71,7 @@ pub fn wireframe(obj: &RawObj, quad_mode: QuadMode) -> Result<(Vec<Vertex>, Vec<
 
             Ok(())
         }),
-        QuadMode::Keep => polygon_indices(obj, |indices, poly| -> Result<()> {
+        QuadMode::Keep => polygon_indices(obj, color_mode, |indices, poly| -> Result<()> {
             quad_check(poly)?;
 
             add_line(indices, poly[0], poly[1]);
@@ -91,6 +99,7 @@ pub fn lines(obj: &RawObj) -> (Vec<Vertex>, Vec<u16>) {
 /// output indices according to a rule (wireframe, triangles, quads)
 fn polygon_indices(
     obj: &RawObj,
+    color_mode: ColorMode,
     mut f: impl FnMut(&mut Vec<u16>, &[u16]) -> Result<()>,
 ) -> Result<(Vec<Vertex>, Vec<u16>)> {
     let mut indices: Vec<u16> = Vec::new();
@@ -101,12 +110,17 @@ fn polygon_indices(
     for polyon in &obj.polygons {
         poly.clear();
 
+        let iter = match color_mode {
+            ColorMode::Normal => polygon_vert_norm_pairs(polyon),
+            ColorMode::Uv => polygon_vert_uv_pairs(polyon),
+        };
+
         // Deduplicate position/normal index pairs into vertices
-        for pair in polygon_vert_norm_pairs(polyon) {
+        for pair in iter {
             let idx = match vert_compressor.get(&pair).copied() {
                 None => {
                     let idx = vertices.len() as u16;
-                    vertices.push(deref_vertex(pair, obj));
+                    vertices.push(deref_vertex(pair, color_mode, obj));
                     vert_compressor.insert(pair, idx);
                     idx
                 }
@@ -122,9 +136,8 @@ fn polygon_indices(
     Ok((vertices, indices))
 }
 
-// TODO: Replace me with something more... fixed function
 /// Extract (position_idx, Option<uvw_idx>) pairs from a polygone,
-fn polygon_vert_norm_pairs<'a>(
+fn polygon_vert_uv_pairs<'a>(
     poly: &'a Polygon,
 ) -> Box<dyn Iterator<Item = (usize, Option<usize>)> + 'a> {
     match poly {
@@ -135,11 +148,25 @@ fn polygon_vert_norm_pairs<'a>(
     }
 }
 
+/// Extract (position_idx, Option<uvw_idx>) pairs from a polygone,
+fn polygon_vert_norm_pairs<'a>(
+    poly: &'a Polygon,
+) -> Box<dyn Iterator<Item = (usize, Option<usize>)> + 'a> {
+    match poly {
+        Polygon::P(v) => Box::new(v.iter().copied().map(|p| (p, None))),
+        Polygon::PN(v) => Box::new(v.iter().copied().map(|(p, t)| (p, Some(t)))),
+        Polygon::PT(v) => Box::new(v.iter().copied().map(|(p, _)| (p, None))),
+        Polygon::PTN(v) => Box::new(v.iter().copied().map(|(p, _, t)| (p, Some(t)))),
+    }
+}
+
+
 /// Create a vertex from an obj given (position, Option<uvw_idx>)
-fn deref_vertex((p, t): (usize, Option<usize>), obj: &RawObj) -> Vertex {
-    let (u, v, w) = match t {
-        Some(t) => obj.tex_coords[t],
-        None => (1., 1., 1.),
+fn deref_vertex((p, t): (usize, Option<usize>), color_mode: ColorMode, obj: &RawObj) -> Vertex {
+    let (u, v, w) = match (t, color_mode) {
+        (Some(t), ColorMode::Normal) => obj.normals[t],
+        (Some(t), ColorMode::Uv) => obj.tex_coords[t],
+        (None, _) => (1., 1., 1.),
     };
     let (x, y, z, _) = obj.positions[p];
     Vertex {
